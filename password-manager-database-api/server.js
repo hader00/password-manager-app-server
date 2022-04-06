1175b4dc6e682cc2a8cf4b4a013267d758169f1e
Martin Haderka <45767708+hader00@users.noreply.github.com>
2022-04-06 23:21:50 +0200

    Add rate limit and other security measures

--- password-manager-database-api/server.js
+++ password-manager-database-api/server.js
@@ -1,12 +1,24 @@
 require("dotenv").config();
 const express = require("express");
+const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
 const cors = require("cors");
+const xss = require('xss-clean');
 const bodyParser = require('body-parser');
 
 const app = express();
+app.use(helmet());
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(xss())
+
+const limit = rateLimit({
+  max: 100,// max requests
+  windowMs: 60 * 60 * 1000, // 1 Hour of 'ban' / lockout
+  message: 'Too many requests' // message to send
+});
+app.use('/api/password-manager', limit); // Setting limiter on specific route
 
 const router = require("./app/routes/routes");
 app.use('/api/password-manager', router);
@@ -16,11 +28,11 @@ db.sequelize.sync();
 
 const PORT = process.env.NODE_DOCKER_PORT || 8080;
 app.listen(PORT, () => {
-  console.info(`Server is running on port ${process.env.NODE_LOCAL_PORT}.`);
+  console.log(`Server is running on port ${PORT}.`);
 });
 
 app.get('/', (req, res) => {
-  res.send(`Server is running on port ${process.env.NODE_LOCAL_PORT}.`);
+  res.send(`Server is running on port ${PORT}.`);
 })
 
 app.get('/available',  (req, res) => {
